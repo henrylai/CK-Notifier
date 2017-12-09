ecfd20f34624290b8434a8f0eff385120fcb0b91
Henry Lai <bzlai@aol.com>
2017-12-09 11:41:07 -0800

    Added threshold

--- cknotifier.user.js
+++ cknotifier.user.js
@@ -4,23 +4,26 @@
 // @namespace http://henrylai.me
 // @version 0.1
 // @description Notifies on low priced CKs
-// @match https://www.cryptokitties.co/marketplace/sale?orderBy=current_price&orderDirection=asc&search=gen%3A0&sorting=cheap
+// @match https://www.cryptokitties.co/marketplace/sale*
 // @copyright 2017
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
-
+$(document).ready(function() {
+    $("#app").prepend("Set alert threshold value: <input id=\"threshold-value\">");
+});
 
 (function loop() {
     $( ".KittyStatus-note" ).each(function( index ) {
+        var threshold = $("#threshold-value").val();
         var text = $(this).text();
         // Price found
         if(text.indexOf('.') !== -1) {
-            console.log( index + ": " + text );
             // convert string to float
             var price = parseFloat(text.substring(2));
-            console.log( "float is: " + price);
-            if (price < 2.7) {
+            if (price < threshold) {
+				$(this).click();
                 alert("Found kitty price of: " + price);
+                return false;
             }
         }
     });
