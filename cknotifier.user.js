561ca63a1113499aef1e26d7bb83a89c12d259af
Henry Lai <bzlai@aol.com>
2017-12-09 12:48:22 -0800

    Added ON/OFF button, autorefresh, sound alert

--- cknotifier.user.js
+++ cknotifier.user.js
@@ -4,33 +4,55 @@
 // @namespace http://henrylai.me
 // @version 0.1
 // @description Notifies on low priced CKs
-// @match https://www.cryptokitties.co/marketplace/sale*
+// @match https://www.cryptokitties.co/*
 // @copyright 2017
 // @require http://code.jquery.com/jquery-latest.js
+// @grant GM_setValue
+// @grant GM_getValue
 // ==/UserScript==
 $(document).ready(function() {
-    $("#app").prepend("Set alert threshold value: <input id=\"threshold-value\">");
-});
+    var threshold = GM_getValue("threshold");
+    var status = GM_getValue("status") ? true : false;
+    $("#app").prepend("Set alert threshold value: <input id=\"threshold-value\" value=\"" + threshold +"\"\>" +
+                      "<button style=\"border:1px solid black\" type=\"button\" class=\"status\">" + (status ? "Stop": "Start") + "<\/button>");
 
-(function loop() {
-    // found flag
-    var found = false;
-    $( ".KittyStatus-note" ).each(function( index ) {
-        var threshold = $("#threshold-value").val();
-        var text = $(this).text();
-        // Price found
-        if(text.indexOf('.') !== -1) {
-            // convert string to float
-            var price = parseFloat(text.substring(2));
-            if (price < threshold) {
-                found = true;
-				$(this).click();
-                alert("Found kitty price of: " + price);
-                return false;
-            }
+    $( ".status" ).click(function(e) {
+        status = !status;
+        GM_setValue("status", status);
+        $(this).text((status ? "Stop": "Start"));
+        if(status) {
+           loop();
         }
     });
-    if (!found) {
-        setTimeout(loop,5000);
-    }
-})();
+
+    (function loop() {
+        if(status === true) {
+            $( ".KittyStatus-note" ).each(function( index ) {
+                var threshold = $("#threshold-value").val();
+                GM_setValue("threshold", threshold);
+                var text = $(this).text();
+                // Price found
+                if(text.indexOf('.') !== -1) {
+                    // convert string to float
+                    var price = parseFloat(text.substring(2));
+                    if (price < threshold) {
+                        status = false;
+                        GM_setValue("status", status);
+                        $(this).click();
+                        mySound = new Audio('https://www.soundjay.com/button/beep-07.mp3');
+                        mySound.play();
+                        return false;
+                    }
+                }
+            });
+            if (status === true) {
+                setTimeout(loop,5000);
+                setTimeout(function() {
+                    location.reload();
+                }, 10000);
+            }
+        }
+    })();
+});
+
+
